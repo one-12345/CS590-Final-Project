0631b73dd53a2a3213bfea33797026236fe80e8c
Celine <celine.zhang2007@gmail.com>
2026-05-22 14:01:44 -0400

    first phase of absorb (excluding keccakf) done

--- SHA3.java
+++ SHA3.java
@@ -2,6 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class SHA3 {
+    public static String keccakf (String state) {
+        // theta
+        // rho
+        // pi
+        // chi
+        // iota
+        return state;
+    }
     public static String pad (String message, int r) {
         // pad the message with 100...001 until the length is a multiple of r
         if (message.length() == 0) {
@@ -28,12 +36,33 @@ public class SHA3 {
             return message + padding;
         }
     }
-    public static void keccakf () {
-        // theta
-        // rho
-        // pi
-        // chi
-        // iota
+    public static void absorb (String message, int b, int d, int c, int r) {
+        // split the message into n r-bit blocks
+        int n = message.length() / r;
+        String[] blocks = new String[n];
+        for (int i = 0; i < n; i++) {
+            blocks[i] = message.substring(i * r, (i + 1) * r);
+        }
+        // initialize the state as a b-bit array of zeros
+        String state = "";
+        for (int i = 0; i < b; i++) {
+            state = state + "0";
+        }
+        // for each block, absorb it into the state by:
+        // (1) extending it at the end by a string of c zero bits
+        // (2) XORing it with the state
+        // (3) applying the permutation function keccakf to the state to yield a new state
+        for (int i = 0; i < n; i++) {
+            String block = blocks[i];
+            for (int i = 0; i < c; i++) {
+                block = block + "0";
+            }
+            int blockInt = Integer.parseInt(block, 2);
+            int stateInt = Integer.parseInt(state, 2);
+            int newStateInt = blockInt ^ stateInt;
+            state = Integer.toBinaryString(newStateInt);
+        }
+        state = keccakf(state); // TODO: implement keccakf
     }
     public static void main (String[] args) {
         String filename = args[0]; // file should contain a bit string on a single line
