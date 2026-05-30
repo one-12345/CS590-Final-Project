49294c9a8946e2922f1a54537a943cdd682470ad
shashwatgupta01 <shashwatgupta121608@gmail.com>
2026-05-30 09:55:23 -0400

    fixed order in toString conversion

--- SHA3.java
+++ SHA3.java
@@ -32,7 +32,7 @@ public class SHA3 {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 for (int k = 0; k < arr[0][0].length; k++) {
-                    str = arr[i][j][k] + str;
+                    str = str + arr[i][j][k];
                 }
             }
         }
